4998aecae067b7a5fb3c22f7b8a085a2214f846b
sai teja <94728002+Saiteja6393@users.noreply.github.com>
2023-08-25 18:43:38 +0530

    Add files via upload

---
+++ TexttoSpeech.py
@@ -0,0 +1,9 @@
+from gtts import gTTS 
+import os
+
+file = open("abc.txt", 'r', encoding='utf-8').read()
+speech = gTTS(text=file, lang='te', slow=False)
+speech.save("voice.mp3")
+os.system("voice.mp3")
+
+#print(file)
